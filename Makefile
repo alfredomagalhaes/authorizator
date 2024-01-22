4259579752f450a2584ad6ca92d0690621356d6d
Alfredo Magalhães <alfredo.magalhaes@outlook.com>
2024-01-22 20:13:48 -0300

    Add initial version of the api server, with no functionalities

---
+++ Makefile
@@ -0,0 +1,5 @@
+build:
+	@go build -o bin/server
+
+run:
+	./bin/server initApiServer
---
+++ cmd/initApiServer.go
@@ -0,0 +1,70 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/gofiber/contrib/fiberzerolog"
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
+)
+
+// initApiServerCmd represents the initApiServer command
+var initApiServerCmd = &cobra.Command{
+	Use:   "initApiServer",
+	Short: "Initialize the API Server to Access control service.",
+	Long: `This application will manage users and their roles to grant/deny
+	access to other microservices `,
+	Run: func(cmd *cobra.Command, args []string) {
+
+		logger := zerolog.New(os.Stderr).With().Timestamp().Logger()
+
+		app := fiber.New()
+
+		app.Use(fiberzerolog.New(fiberzerolog.Config{
+			Logger: &logger,
+		}))
+
+		// Listen from a different goroutine
+		go func() {
+			if err := app.Listen(":3000"); err != nil {
+				log.Panic(err)
+			}
+		}()
+
+		c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+		signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+		_ = <-c // This blocks the main thread until an interrupt is received
+		fmt.Println("Gracefully shutting down...")
+		_ = app.Shutdown()
+
+		fmt.Println("Running cleanup tasks...")
+
+		// Your cleanup tasks go here
+		// db.Close()
+		// redisConn.Close()
+		fmt.Println("Fiber was successful shutdown.")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initApiServerCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initApiServerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initApiServerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
